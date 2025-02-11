94fa59a6280c0df30ab7861f91a99bb8607fe28a
umar <mdumarkhan.1207@gmail.com>
2025-02-11 21:51:33 +0530

    add: init command

--- app/main.js
+++ app/main.js
@@ -7,8 +7,12 @@ case "init":
         init();
         break;
     case "add":
+        // create a blob object with compressed representations of each file
         const file = process.argv[3];
         add(file);
+        break;
+    case "commit":
+        // create a commit and tree object with some meta data
         break;
     default:
         console.log("error");
