27113fecab53b5da44b1339e9743d42328b5530f
umar <mdumarkhan.1207@gmail.com>
2025-02-11 20:06:02 +0530

    starter template

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # git-lite
+
+## usage
+
+```sh
+git clone https://github.com/umar1207/git-lite.git
+```
+
+### add this to your bashrc or zshrc
+```sh
+alias mygit = /this/repo/path/main.sh
+```
+
+### use mygit instead of git for the following commands
+
+```sh
+mygit init
+mygit add filename/.
+mygit commit
+```
--- app/main.js
+++ app/main.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+const arg1 = process.argv[2];
+
+switch (arg1) {
+    case "init":
+        console.log("intialised");
+        // call function to init a repo
+        break;
+    case "add":
+        const file = process.argv[3];
+        if(!file) {
+            console.log("No files passed");
+            break;
+        }
+        // function to git add
+        console.log("added the files to staging area");
+        break;
+    default:
+        console.log("error");
+        break;
+}
